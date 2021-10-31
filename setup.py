5fd0af42fd3f9eb18f667861519820a5a99871d3
Roland van Laar <roland@rolandvanlaar.nl>
2021-10-31 21:48:17 +0100

    Upgrade to tactic 4.8
    Changes to the client packaging:
    
        - tactic is imported as a submodule. Should make it easier to update
          it later
        - python3 is marked as supported
        - update setup.cfg, to allow for a universal wheel
        - no more [ci skip] in the commit messages with zest.releaser
        - scripts were moved to the bin directory by upstream tactic
        - removed description from setup.py, should be picked up
          automatically

--- setup.py
+++ setup.py
@@ -6,30 +6,17 @@ from setuptools import setup, find_packages
 
 version = '4.8.dev0'
 
-def read(filename):
-    try:
-        with codecs.open(filename, encoding='utf-8') as f:
-            return unicode(f.read())
-    except NameError:
-        with open(filename, 'r', encoding='utf-8') as f:
-            return f.read()
-
-
-
-long_description = read('README.rst').encode('utf-8')
-
 setup(name='tactic_client_lib',
       version=version,
       install_requires=['six'],
       description='Tactic Client Library',
-      long_description=long_description,
       author='Roland van Laar',
       author_email='roland@micite.net',
       url='https://github.com/rvanlaar/tactic-client',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      scripts=['bin/get_ticket.py', 'create_project.py'],
+      scripts=['bin/get_ticket.py', 'bin/create_project.py'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved',
